04dc6adc1b63875555ff2b0ebd205fa1c0083527
Aeek True <aeektrue@gmail.com>
2022-01-29 18:28:20 +0300

    change IS_BINARY_FILE condition

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ IS_BINARY_FILE = False
 
 
 if IS_BINARY_FILE:
+	HOME_DIR = os.environ.get("HOME")
 	FACTORY_DIR = os.path.join(sys._MEIPASS, "data")
+	WORK_DIR = os.path.join(HOME_DIR, ".fresh-reader")
 else:
 	FACTORY_DIR = "data"
-
-HOME_DIR = os.environ.get("HOME")
-WORK_DIR = os.path.join(HOME_DIR, ".fresh-reader")
+	WORK_DIR = "data"
 
 TEMPLATE_FILE = os.path.join(WORK_DIR, "template.html")
 DEMO_FILE = os.path.join(WORK_DIR, "demo.html")
 STYLE_FILE = os.path.join(WORK_DIR, "style.css")
 SCRIPT_FILE = os.path.join(WORK_DIR, "script.js")
-OUTPUT_FILE = os.path.join(WORK_DIR, "output.html")
+OUTPUT_FILE = os.path.join(WORK_DIR, "fiction-book.html")
