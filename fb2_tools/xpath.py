3bfbaee2d482342021b85ab6048b3bb14e2eb8e8
Aeek True <aeektrue@gmail.com>
2022-02-06 00:44:30 +0300

    xpath Binary added

--- fb2_tools/xpath.py
+++ fb2_tools/xpath.py
@@ -2,8 +2,8 @@ from . import tags
 
 BODY = f"./{tags.BODY}"
 NOTES = f"./{tags.BODY}[@name='notes']"
-TITLE_INFO = f"./{tags.DESCRIPTION}/{tags.TITLE_INFO}"
-
+BINARY = f"./{tags.BINARY}"
 DESCRIPTION = f"./{tags.DESCRIPTION}"
+TITLE_INFO = f"./{tags.DESCRIPTION}/{tags.TITLE_INFO}"
 DOCUMENT_INFO = f"{DESCRIPTION}/{tags.DOCUMENT_INFO}"
 PUBLISH_INFO = f"{DESCRIPTION}/{tags.PUBLISH_INFO}"
