64188e52a7fa1e50d90d337612b30fd3a215c689
Aeek True <aeektrue@gmail.com>
2022-02-13 15:05:43 +0000

    refactor dirs detecting

--- config.py
+++ config.py
@@ -1,13 +1,18 @@
 import os
+import sys
 
 NAME = "fresh-reader"
-VERSION = "0.2.0"
-
+VERSION = "0.3.0"
 HOME_DIR = os.path.expanduser("~")
 USER_DATA_DIR = os.path.join(HOME_DIR, ".fresh-reader")
-
 OUTPUT_FILE = os.path.join(USER_DATA_DIR, "fiction-book.html")
-WORK_DIR = "data"
+
+if getattr(sys, 'frozen', False):
+	APP_PATH = os.path.dirname(sys.executable)
+else:
+	APP_PATH = os.path.dirname(__file__)
+
+WORK_DIR = os.path.join(APP_PATH, "data")
 DEMO_FILE = os.path.join(WORK_DIR, "demo.fb2")
 
 # path relative to WORK_DIR
